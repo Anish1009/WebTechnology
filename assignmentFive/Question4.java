79b2c1098a210c1666965db7c4a35735a8660ace
Anish1009 <78020886+Anish1009@users.noreply.github.com>
2022-11-28 17:07:55 +0530

    Update Question4.java

--- assignmentFive/Question4.java
+++ assignmentFive/Question4.java
@@ -1,18 +1,19 @@
 public class Question4{
-static int a = 20;
-Myclass() {
-a++;
+    static int a = 20;
+    Question4() {
+    a++;
+    }
+    void m1() {
+    a++;
+    System.out.println(a);
+    }
+    public static void main(String[]
+    args)
+    {
+    Question4 obj = new Question4();
+    Question4 obj2 = new Question4();
+    Question4 obj3 = new Question4();
+    obj3.m1();
+    }
 }
-Void m1() {
-a++;
-System.out.println(a):
-}
-public static void main(String[]
-args)
-{
-Myclass obj = new Myclass();
-Myclass obj2 = new Myclass();
-Myclass obj3 = new Myclass();
-obj3.m1();
-}
-}
+// Output : 24
